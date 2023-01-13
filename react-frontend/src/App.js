0e31747a848dd95bc3761e952dfb5e6483869456
Lucas Barga <lucasmpbarga@gmail.com>
2023-01-12 23:50:01 -0300

    add frontend project

---
+++ react-frontend/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div >
+          {`localStorage: `}
+          {Object.entries(localStorage).map(entrie => <div>
+            <br />
+            {`${entrie[0]}: ${entrie[1]}`}
+          </div>)}
+        </div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
